ec5176a3a0b142c8641f7f90ca38f7763f3ec8ae
pinnguino <s.gallardogaston@gmail.com>
2025-06-27 00:04:50 -0300

    Adding lazy.nvim and first plugins (colorschemes)

---
+++ lua/user/lazy.lua
@@ -0,0 +1,33 @@
+-- Install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Remap space as leader key. Mapping the leader key before loading lazy is required.
+vim.keymap.set("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Load plugins
+require("lazy").setup({
+	spec = {
+		-- Load plugins from sections
+		{ import = "user.plugins.colorschemes" },
+	},
+	ui = {
+		border = "rounded"
+	},
+	change_detection = {
+		enabled = true,
+		notify = true -- Recieve a notification when the configuration changes
+	}
+})
