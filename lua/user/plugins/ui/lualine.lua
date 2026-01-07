eb9706f9a642ce58da716cbf2c1a822bcf0ddb93
pinnguino <s.gallardogaston@gmail.com>
2026-01-07 18:48:13 -0300

    Customize the look of lualine

--- lua/user/plugins/ui/lualine.lua
+++ lua/user/plugins/ui/lualine.lua
@@ -7,8 +7,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = 'auto',
-				component_separators = { left = '', right = ''},
-				section_separators = { left = '', right = ''},
+				component_separators = { left = '│', right = '│'},
+				section_separators = { left = '', right = ''},
 				disabled_filetypes = {
 					'NvimTree'
 				},
@@ -36,8 +36,18 @@ return {
 				}
 			},
 			sections = {
-				lualine_a = {'mode'},
-				lualine_b = {'branch', 'diff', 'diagnostics'},
+				lualine_a = {
+					{
+						'mode',
+						gui = 'bold', -- Bold text mode
+					},
+				},
+				lualine_b = {
+					{
+						'branch',
+						icon = ''
+					},
+					'diff', 'diagnostics'},
 				lualine_c = {'filename'},
 				lualine_x = {'filetype'},
 				lualine_y = {},
