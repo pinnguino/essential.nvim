6d68f66515888bacf2c32de5109ce58ab06629ba
pinnguino <s.gallardogaston@gmail.com>
2026-01-07 18:36:02 -0300

    Add keymap for selecting a file within fzf-lua

--- lua/user/plugins/core/fzf.lua
+++ lua/user/plugins/core/fzf.lua
@@ -2,5 +2,13 @@ return {
 	"ibhagwan/fzf-lua",
 	event = "VeryLazy",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {}
+	opts = function()
+		return {
+			defaults = {
+				actions = {
+					["ctrl-l"] = require("fzf-lua").actions.file_edit,
+				}
+			}
+		}
+	end
 }
