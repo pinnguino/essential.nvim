7eb96e52061b72c3255b92a4700b6edca3a37efe
pinnguino <s.gallardogaston@gmail.com>
2026-01-16 21:09:01 -0300

    Add nvim-notify plugin

---
+++ lua/user/plugins/ui/notify.lua
@@ -0,0 +1,11 @@
+return {
+	'rcarriga/nvim-notify',
+	event = "VeryLazy",
+	config = function()
+		require("notify").setup({
+			stages = 'fade_in_slide_out',
+			timeout = 2000,
+		})
+		vim.notify = require('notify')
+	end
+}
