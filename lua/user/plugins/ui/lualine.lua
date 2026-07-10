cf32040ef28f69f5f8ad8df4a83fc457aef0c8e7
pinnguino <s.gallardogaston@gmail.com>
2026-07-10 15:01:54 -0300

    Add dashboard plugin with custom headers

--- lua/user/plugins/ui/lualine.lua
+++ lua/user/plugins/ui/lualine.lua
@@ -10,7 +10,8 @@ return {
 				component_separators = { left = '│', right = '│'},
 				section_separators = { left = '', right = ''},
 				disabled_filetypes = {
-					'NvimTree'
+					'NvimTree',
+					'dashboard'
 				},
 				ignore_focus = {},
 				always_divide_middle = true,
