d4720a1aaa0e5ecb091abf9efb192ee2ae2b97c6
pinnguino <s.gallardogaston@gmail.com>
2026-07-10 14:57:38 -0300

    Pin treesitter commit and add new parsers

--- lua/user/plugins/core/treesitter.lua
+++ lua/user/plugins/core/treesitter.lua
@@ -2,21 +2,22 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	lazy = false,
 	build = ':TSUpdate', -- Automatically update parsers on plugin update
+	commit = '7caec27', -- Commit that still has 0.11 support
 	config = function()
 		require('nvim-treesitter').setup ({
-			ensure_installed = {
+			ensure_installed = { -- Languages to install parsers for
 				"lua",
 				"vimdoc",
 				"markdown",
 				"markdown_inline",
 				"html",
 				"css",
+				"javascript",
 				"bash",
 				"git_config",
 				"gitignore",
-				"json",
-
-			}, -- Languages to install parsers for
+				"json"
+			},
 			highlight = {
 				enable = true, -- Enable syntax highlighting
 			},
